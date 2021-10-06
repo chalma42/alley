901be30eb2fe3f980b342e1875f0ed368839c288
Alexandr Yakovlev <chalma123@yandex.ru>
2021-10-06 23:30:15 +0400

    06-10-2021-2

--- blocks/afisha.php
+++ blocks/afisha.php
@@ -1,48 +1,336 @@
 <div class="swiper-container">
-				  <div class="swiper-wrapper">
-					<div class="swiper-slide">
-							<a href="poster_page.php">
-							<div class="poster_item">
-								<div class="poster_item_img">
-									<img src="img/afisha/2021/09/Weekend.jpg">
-								</div>
-								<div class="poster_properties flex">
-									<div class="label_logo dark_bg">
-										<div class="label_logo_wrapper">
-										<img src="/img/logos/tryfel_logo.png">
-										</div>
-									</div>
-									<div class="prop">
-										<span>Мастер-класс</span>
-										<div class="date">10 сентября</div>
-									</div>
-								</div>
-								<div class="poster_title">WEEKEND, Америка</div>
-								<div class="poster_anons">Рибай стейк...</div>
-							</div>
-							</a>
-						</div>
-					<div class="swiper-slide">
-				  		<a href="poster_page.php">
-					  	<div class="poster_item">
-							<div class="poster_item_img">
-						    	<img src="img/afisha/2021/09/Bon-appetit.jpg">
-						    </div>
-						    <div class="poster_properties flex">
-						    	<div class="label_logo dark_bg">
-						    		<div class="label_logo_wrapper">
-                                        <img src="/img/logos/tryfel_logo.png">
-						    		</div>
-						    	</div>
-						    	<div class="prop">
-						    		<span>Мастер-класс</span>
-						    		<div class="date">11 сентября</div>
-						    	</div>
-						    </div>
-						    <div class="poster_title">BON APPETIT, Франция</div>
-						    <div class="poster_anons">Куриная грудка со сливочно-цитрусовым соусом...</div>
-						</div>
-						</a>
+	<div class="swiper-wrapper">
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/1.jpg">
 					</div>
-				  </div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+							<div class="label_logo_wrapper">
+								<img src="/img/logos/tryfel_logo.png">
+							</div>
+						</div>
+						<div class="prop">
+							<span>Мастер-класс</span>
+							<div class="date">01 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">ИТАЛИЯ В КРАСКАХ, Италия</div>
+					<div class="poster_anons">...</div>
 				</div>
+			</a>
+		</div>
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/2.jpg">
+					</div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+							<div class="label_logo_wrapper">
+								<img src="/img/logos/tryfel_logo.png">
+							</div>
+						</div>
+						<div class="prop">
+							<span>Мастер-класс</span>
+							<div class="date">02 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">Отдых по-американски, США</div>
+					<div class="poster_anons">...</div>
+				</div>
+			</a>
+		</div>
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/3.jpg">
+					</div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+							<div class="label_logo_wrapper">
+								<img src="/img/logos/tryfel_logo.png">
+							</div>
+						</div>
+						<div class="prop">
+							<span>Мастер-класс</span>
+							<div class="date">03 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">Селяви, Франция</div>
+					<div class="poster_anons">...</div>
+				</div>
+			</a>
+		</div>
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/8.jpg">
+					</div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+							<div class="label_logo_wrapper">
+								<img src="/img/logos/tryfel_logo.png">
+							</div>
+						</div>
+						<div class="prop">
+							<span>Мастер-класс</span>
+							<div class="date">08 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">Ужин у шефа, Франция</div>
+					<div class="poster_anons">...</div>
+				</div>
+			</a>
+		</div>
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/Bonappetit.jpg">
+					</div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+							<div class="label_logo_wrapper">
+								<img src="/img/logos/tryfel_logo.png">
+							</div>
+						</div>
+						<div class="prop">
+							<span>Мастер-класс</span>
+							<div class="date">09 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">Bon appetit, Франция</div>
+					<div class="poster_anons">...</div>
+				</div>
+			</a>
+		</div>
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/MAMAMIA.jpg">
+					</div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+						    <div class="label_logo_wrapper">
+                                <img src="/img/logos/tryfel_logo.png">
+						    </div>
+						</div>
+						<div class="prop">
+						    <span>Мастер-класс</span>
+						    <div class="date">10 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">MAMA MIA, Италия</div>
+					<div class="poster_anons">...</div>
+				</div>
+			</a>
+		</div>
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/15.jpg">
+					</div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+						    <div class="label_logo_wrapper">
+                                <img src="/img/logos/tryfel_logo.png">
+						    </div>
+						</div>
+						<div class="prop">
+						    <span>Мастер-класс</span>
+						    <div class="date">15 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">Дары Ниццы, Франция</div>
+					<div class="poster_anons">...</div>
+				</div>
+			</a>
+		</div>
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/16.jpg">
+					</div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+						    <div class="label_logo_wrapper">
+                                <img src="/img/logos/tryfel_logo.png">
+						    </div>
+						</div>
+						<div class="prop">
+						    <span>Мастер-класс</span>
+						    <div class="date">16 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">Свидание в Милане, Италия</div>
+					<div class="poster_anons">...</div>
+				</div>
+			</a>
+		</div>
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/17.jpg">
+					</div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+						    <div class="label_logo_wrapper">
+                                <img src="/img/logos/tryfel_logo.png">
+						    </div>
+						</div>
+						<div class="prop">
+						    <span>Мастер-класс</span>
+						    <div class="date">17 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">Weekend, США</div>
+					<div class="poster_anons">...</div>
+				</div>
+			</a>
+		</div>
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/22.jpg">
+					</div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+						    <div class="label_logo_wrapper">
+                                <img src="/img/logos/tryfel_logo.png">
+						    </div>
+						</div>
+						<div class="prop">
+						    <span>Мастер-класс</span>
+						    <div class="date">22 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">Легкий Французский ужин, Франция</div>
+					<div class="poster_anons">...</div>
+				</div>
+			</a>
+		</div>
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/23.jpg">
+					</div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+						    <div class="label_logo_wrapper">
+                                <img src="/img/logos/tryfel_logo.png">
+						    </div>
+						</div>
+						<div class="prop">
+						    <span>Мастер-класс</span>
+						    <div class="date">23 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">Мясной пир, США</div>
+					<div class="poster_anons">...</div>
+				</div>
+			</a>
+		</div>
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/24.jpg">
+					</div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+						    <div class="label_logo_wrapper">
+                                <img src="/img/logos/tryfel_logo.png">
+						    </div>
+						</div>
+						<div class="prop">
+						    <span>Мастер-класс</span>
+						    <div class="date">24 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">Вива Италия, Италия</div>
+					<div class="poster_anons">...</div>
+				</div>
+			</a>
+		</div>
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/29.jpg">
+					</div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+						    <div class="label_logo_wrapper">
+                                <img src="/img/logos/tryfel_logo.png">
+						    </div>
+						</div>
+						<div class="prop">
+						    <span>Мастер-класс</span>
+						    <div class="date">29 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">Вечерняя Сицилия, Италия</div>
+					<div class="poster_anons">...</div>
+				</div>
+			</a>
+		</div>
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/30.jpg">
+					</div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+						    <div class="label_logo_wrapper">
+                                <img src="/img/logos/tryfel_logo.png">
+						    </div>
+						</div>
+						<div class="prop">
+						    <span>Мастер-класс</span>
+						    <div class="date">30 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">Вечер на Манхэттене, США</div>
+					<div class="poster_anons">...</div>
+				</div>
+			</a>
+		</div>
+		<div class="swiper-slide">
+			<a href="poster_page.php">
+				<div class="poster_item">
+					<div class="poster_item_img">
+						<img src="img/afisha/2021/10/31.jpg">
+					</div>
+					<div class="poster_properties flex">
+						<div class="label_logo dark_bg">
+						    <div class="label_logo_wrapper">
+                                <img src="/img/logos/tryfel_logo.png">
+						    </div>
+						</div>
+						<div class="prop">
+						    <span>Мастер-класс</span>
+						    <div class="date">31 октября</div>
+						</div>
+					</div>
+					<div class="poster_title">Осень по французски, Франция</div>
+					<div class="poster_anons">...</div>
+				</div>
+			</a>
+		</div>
+
+
+	</div>
+</div>
